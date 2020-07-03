8458891997100babd1af0ddfefc45f191f77ed0a
Sandra-aguilar <62567980+Sandra-aguilar@users.noreply.github.com>
2020-07-03 03:37:57 -0500

    Add files via upload

---
+++ ContadordeLetras.java
@@ -0,0 +1,25 @@
+  
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contadordeletras;
+
+/**
+ *
+ * @author User
+ */
+public class ContadordeLetras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Interfaz c1=new Interfaz();
+        
+    c1.setVisible(true);
+    
+    }
+        }
+   
